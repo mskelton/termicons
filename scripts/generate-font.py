38644dc02a37ca4cefe72175091e64f109446513
Mark Skelton <mdskelton99@gmail.com>
2023-02-19 17:31:46 -0600

    Finish script

--- scripts/generate-font.py
+++ scripts/generate-font.py
@@ -4,7 +4,7 @@ import fontforge
 
 version = "1.0.0"
 font_name = "devicons"
-start_codepoint = 0xFE00
+start_codepoint = 0xEC00
 output_dir = "dist"
 
 font = fontforge.font()
@@ -12,8 +12,9 @@ font.fontname = font_name
 font.familyname = font_name
 font.fullname = font_name
 font.encoding = "UnicodeFull"
-font.em = 512
+font.em = 1024
 
+font.sfntRevision = None # Auto-set (refreshed) by fontforge
 font.version = version
 font.copyright = "Mark Skelton"
 
@@ -22,34 +23,18 @@ font.appendSFNTName(en, "Version", version)
 font.appendSFNTName(en, "Vendor URL", "https://github.com/mskelton/devicons")
 font.appendSFNTName(en, "Copyright", "Mark Skelton")
 
-# font.autoWidth(0, 0, font.em)
-
 # Add valid space glyph to avoid "unknown character" box on IE11
 glyph = font.createChar(32)
 glyph.width = 200
 
-output_info = {
-    "em": font.em,
-    "icons": {},
-}
-
-def addIcon(icon):
-    name = icon["name"]
-
-    glyph = font.createChar(int(icon["codepoint"]), name)
-    glyph.importOutlines(
-        os.path.join("svgs", (name + ".svg")),
-    )
+def addIcon(offset: int, name: str):
+    glyph = font.createChar(start_codepoint + offset, name)
+    glyph.importOutlines(os.path.join("svgs", (name + ".svg")))
     glyph.left_side_bearing = 0
     glyph.right_side_bearing = 0
 
-    output_info["icons"]["i_" + name] = {
-        "width": glyph.width,
-    }
-
-
 with open("icons.csv") as file:
-    for icon in csv.DictReader(file):
-        addIcon(icon)
+    for icon in csv.reader(file):
+        addIcon(int(icon[0]), icon[1])
 
 font.generate(os.path.join(output_dir, font_name + ".otf"))
